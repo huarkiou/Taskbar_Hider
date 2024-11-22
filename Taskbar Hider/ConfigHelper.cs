ee6a604707a64052d76251aec8576993f9db25d4
huarkiou <huarkiou@outlook.com>
2024-11-22 20:51:02 +0800

    修复没能正确写入json文件的bug

--- Taskbar Hider/ConfigHelper.cs
+++ Taskbar Hider/ConfigHelper.cs
@@ -7,15 +7,15 @@ namespace Tbh;
 
 internal struct Configuration()
 {
-    internal HOT_KEY_MODIFIERS Modifiers { get; set; } = HOT_KEY_MODIFIERS.MOD_ALT;
-    internal VIRTUAL_KEY VKey { get; set; } = VIRTUAL_KEY.VK_F2;
-    internal bool ShowTaskbarOnStartup { get; set; } = true;
+    public HOT_KEY_MODIFIERS Modifiers { get; set; } = HOT_KEY_MODIFIERS.MOD_ALT;
+    public VIRTUAL_KEY VKey { get; set; } = VIRTUAL_KEY.VK_F2;
+    public bool ShowTaskbarOnStartup { get; set; } = true;
 }
 
 internal static class ConfigHelper
 {
     private static readonly string ConfigPath =
-        Path.GetDirectoryName(Environment.ProcessPath) + @"\" + App.ProgramName + ".json";
+        Path.GetDirectoryName(Environment.ProcessPath) + @"/" + App.ProgramName + ".json";
 
     public static Configuration Config = new();
 
