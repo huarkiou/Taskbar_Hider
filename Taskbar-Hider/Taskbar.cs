1c13f04c04b904a5ad832790835358f975a3a375
huarkiou <huarkiou@outlook.com>
2024-11-23 23:41:36 +0800

    增加Pinvoke.IsWindow，对任务栏句柄的合法性进行检查

--- Taskbar-Hider/Taskbar.cs
+++ Taskbar-Hider/Taskbar.cs
@@ -12,69 +12,44 @@ namespace Taskbar_Hider_Avalonia;
 
 internal class Taskbar
 {
+    public bool Visibility { get; set; }
+
     private readonly bool _defaultAutoHide;
     private readonly DispatcherTimer _timer;
+    private HWND HWnd { get; set; }
+    private const string ClassName = "Shell_TrayWnd";
 
     public Taskbar()
     {
-        // 设置任务栏句柄
-        HWnd = HWND.Null;
-        HWnd = PInvoke.FindWindow("System_TrayWnd", null);
-        if (HWnd == HWND.Null)
-        {
-            var sw = new Stopwatch();
-            sw.Start();
-            while (HWnd == HWND.Null)
-            {
-                HWnd = PInvoke.FindWindow("Shell_TrayWnd", null);
-                if (sw.Elapsed == new TimeSpan(0, 0, 5)) throw new Exception("找不到任务栏句柄");
-            }
-        }
+        ResetHandle();
 
         // 检查并保存当前任务栏状态
-        var taskbarState = GetTaskbarState();
-        _defaultAutoHide = taskbarState is PInvoke.ABS_AUTOHIDE
-            or (PInvoke.ABS_AUTOHIDE | PInvoke.ABS_ALWAYSONTOP);
+        _defaultAutoHide = (GetTaskbarState() & PInvoke.ABS_AUTOHIDE) == PInvoke.ABS_AUTOHIDE;
         Visibility = PInvoke.IsWindowVisible(HWnd);
 
-        // 设置定时器用于定时检查任务栏的隐藏情况
+        // 设置定时器用于定时保证任务栏的隐藏情况
         _timer = new DispatcherTimer
         {
-            Interval = new TimeSpan(0, 0, 0, 0, 500)
+            Interval = new TimeSpan(0, 0, 0, 0, 1000)
         };
-        _timer.Tick += OnTimerHandler;
+        _timer.Tick += (_, _) =>
+        {
+            if (PInvoke.IsWindowVisible(HWnd) == Visibility) Show(Visibility);
+        };
         _timer.Start();
     }
 
-    public bool Visibility { get; set; }
-
-    private HWND HWnd { get; set; }
-
-    private void OnTimerHandler(object? sender, EventArgs e)
-    {
-        if (PInvoke.IsWindowVisible(HWnd) == Visibility) Show(Visibility);
-    }
-
-
     public void ResetHandle()
     {
-        HWnd = PInvoke.FindWindow("System_TrayWnd", null);
+        HWnd = PInvoke.FindWindow(ClassName, null);
         if (HWnd != HWND.Null) return;
         Stopwatch sw = new();
         sw.Start();
         while (HWnd == HWND.Null)
         {
-            HWnd = PInvoke.FindWindow("Shell_TrayWnd", null);
-            if (sw.Elapsed == new TimeSpan(0, 0, 5)) throw new Exception("找不到任务栏句柄");
+            HWnd = PInvoke.FindWindow(ClassName, null);
+            if (sw.Elapsed > new TimeSpan(0, 0, 5)) throw new Exception("找不到任务栏句柄");
         }
-    }
-
-    private void Show(bool show)
-    {
-        PInvoke.ShowWindow(HWnd,
-            show
-                ? SHOW_WINDOW_CMD.SW_SHOW
-                : SHOW_WINDOW_CMD.SW_HIDE);
     }
 
     public void ChangeState()
@@ -89,10 +64,19 @@ internal class Taskbar
         AppConfiguration.Instance.Config.ShowTaskbarOnStartup = Visibility;
     }
 
-    /// <summary>
-    ///     Set the Taskbar State option
-    /// </summary>
-    /// <param name="alwaysOnTop"></param>
+    private void Show(bool show)
+    {
+        if (!PInvoke.IsWindow(HWnd))
+        {
+            ResetHandle();
+        }
+
+        PInvoke.ShowWindow(HWnd,
+            show
+                ? SHOW_WINDOW_CMD.SW_SHOW
+                : SHOW_WINDOW_CMD.SW_HIDE);
+    }
+
     private void SetTaskbarState(bool alwaysOnTop)
     {
         var msgData = new APPBARDATA();
@@ -104,10 +88,6 @@ internal class Taskbar
         PInvoke.SHAppBarMessage(PInvoke.ABM_SETSTATE, ref msgData);
     }
 
-    /// <summary>
-    ///     Gets the current Taskbar state
-    /// </summary>
-    /// <returns>current Taskbar state</returns>
     private uint GetTaskbarState()
     {
         var msgData = new APPBARDATA();
@@ -115,5 +95,4 @@ internal class Taskbar
         msgData.hWnd = HWnd;
         return PInvoke.SHAppBarMessage(PInvoke.ABM_GETSTATE, ref msgData).ToUInt32();
     }
-} // class Taskbar
-// namespace Taskbar_Hider
+}
